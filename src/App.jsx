388f611a5125b8f0dceaefc9e7c2e93c3f2b4a34
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-06-18 00:36:05 +0200

    test netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PreviewGrid from './components/PreviewGrid';
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Render the Navigation component */}
+      <Navbar /> 
       <main>
         <PreviewGrid />
       </main>
