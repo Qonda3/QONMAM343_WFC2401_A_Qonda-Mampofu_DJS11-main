21d12eee612611b0d018c462e6ad574f27e08b25
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-06-18 00:18:25 +0200

    correcting the dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PreviewGrid from '/components/PreviewGrid';
-import Navbar from '/components/Navbar'; 
+import PreviewGrid from './components/PreviewGrid';
+import Navbar from './components/Navbar'; 
 
 export default function App() {
   return (
