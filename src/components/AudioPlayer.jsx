d7c195a45494bb3d90b4ebbbc3b54f7df968b4eb
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-06-20 13:36:36 +0200

    removed a play/pause button n audio placeholder

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -68,12 +68,6 @@ const AudioPlayer = ({ episode, seasonImage, shouldShowPlayer }) => {
         >
           <FontAwesomeIcon icon={isLiked ? solidHeart : regularHeart} size="lg" />
         </button>
-        <button
-          onClick={togglePlay}
-          className="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600 focus:outline-none"
-        >
-          <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
-        </button>
       </div>
     </div>
   );
